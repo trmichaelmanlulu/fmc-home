b24f309d3572d3a89a3d05e9d630811cbfbc9f6c
Michael Manlulu <michael.manlulu@tooltwist.com>
2018-05-01 11:06:37 +0800

    added tablet sidebar

--- js/script.js
+++ js/script.js
@@ -45,11 +45,33 @@ $(function() { // On ready
     });
 
     /* Carousel buttons */
-    $('.sg_carousel-prev').on('click', function() {
+    $('.sg_carousel-prev').on('click', function(e) {
         $('.sg_carousel').slick('slickPrev');
+        e.preventDefault();
     });
-    $('.sg_carousel-next').on('click', function() {
+    $('.sg_carousel-next').on('click', function(e) {
         $('.sg_carousel').slick('slickNext');
+        e.preventDefault();
+    });
+
+    /* Sidebar behavior */
+    // Slide In
+    $('.sidebar-actions a, .sidebar-items a').on('click', function(e) {
+        var currentTab = $(this).attr('data-tabs');
+        $(currentTab).addClass('slideIn');
+        e.preventDefault();
+    });
+
+    // Slide Out
+    $('.sidebar-contents-back').on('click', function(e) {
+        var currentTab = $(this).attr('data-tabs');
+        $(currentTab).removeClass('slideIn');
+        e.preventDefault();
+    });
+
+    $('.sidebar-close').on('click', function(e) {
+        $('.header-sidebar').removeClass('slideIn');
+        e.preventDefault();
     });
 
 });
